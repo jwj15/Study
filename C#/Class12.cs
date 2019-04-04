54341b285313f2e3f47496cf3f3200af0317260c
jwj15 <jangwookjin100@gmail.com>
2019-04-04 14:54:01 +0900

    추가

--- C#/Class12.cs
+++ C#/Class12.cs
@@ -1,25 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Test1
+namespace Study
 {
-    class Program
+    class Class12
     {
-        static void Main(string[] args)
+        // 네트워크
+
+        static void Main()
         {
+            // ip주소 표현 방식
+            IPAddress ip1 = IPAddress.Parse("192.168.1.13");
 
-            // 코드 퍼스트 방식 중 데이터베이스 가져오고 난 후 코드 수정 후 DB 업데이트 하는법
+            IPAddress ip2 = new IPAddress(new byte[] { 192, 168, 1, 13 });
 
-            // 도구 -> Nuget패키지 관리자 -> 패키지 관리자 콘솔
-            // Enable-Migrations
-            // Add-Migration InitialCreate -IgnoreChanges
-            // update-database
-            // 수정후
-            // Add-Migration AddColumn
-            // update-database
+            IPAddress ip3 = new IPAddress(218212544);
+            Console.WriteLine("{0}\n{1}\n{2}", ip1, ip2, ip3.ToString());
+
+            // 유용한 IPAddress 메서드
+            IPAddress ip = IPAddress.Parse("216.58.216.174");
+
+            byte[] ipbytes = ip.GetAddressBytes(); // IP를 바이트배열로 
+
+            IPAddress ipv6 = ip.MapToIPv6();  // IPv4를 IPv6로 매핑 
+            Console.WriteLine(ipv6);
+
+            // 인터넷 호스트명 정보 얻기
+            IPHostEntry hostEntry = Dns.GetHostEntry("www.google.com");
+
+            Console.WriteLine(hostEntry.HostName);
+            foreach (IPAddress i1 in hostEntry.AddressList)
+            {
+                Console.WriteLine(i1);
+            }
+
+            Console.WriteLine("--------------------------");
+            // 로컬 호스트명 정보 얻기
+            string hostname = Dns.GetHostName();
+            IPHostEntry localhost = Dns.GetHostEntry(hostname);
+
+            foreach (IPAddress i1 in localhost.AddressList)
+            {
+                Console.WriteLine(i1);
+            }
+            Console.WriteLine("--------------------------");
+
+            // IP 에서 호스트명 알아내기
+            IPAddress ipaddr = IPAddress.Parse("172.217.161.36");
+            IPHostEntry hostEntry2 = Dns.GetHostEntry(ipaddr);
+            Console.WriteLine(hostEntry2.HostName);
+
+
+            IPAddress ip5 = IPAddress.Parse("74.125.28.99");
+            IPEndPoint ep = new IPEndPoint(ip5, 80); // 포트
+
+            Console.WriteLine(ep.ToString());
+
         }
+
+
     }
 }
---
+++ C#/Class13.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study
+{
+    class Class13
+    {
+        static void Main()
+        {
+            // request, response
+            var uri = "https://www.google.com";
+            var request = WebRequest.Create(uri) as HttpWebRequest;
+            var response = request.GetResponse() as HttpWebResponse;
+            var status = response.StatusCode;
+            
+            // credential
+            var username = "jespera";
+            var password = "Pa$$w0rd";
+            request.Credentials = new NetworkCredential(username, password);
+
+            // data 전송
+            var rawData = Encoding.Default.GetBytes("{\"emailAddress\":\"jesper@fourthcoffee.com\"}");
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            request.ContentLength = rawData.Length;
+            var dataStream = request.GetRequestStream();
+            dataStream.Write(rawData, 0, rawData.Length);
+            dataStream.Close();
+
+            // FTP uploading
+            var uri2 = "ftp://sales.fourthcoffee.com/FileRepository/test.xlsx";
+            var rawData2 = File.ReadAllBytes("C:\\test\\test.xlsx");
+            var request2 = WebRequest.Create(uri2) as FtpWebRequest;
+            request2.Method = WebRequestMethods.Ftp.UploadFile;
+            request2.ContentLength = rawData2.Length;
+            var dataStream2 = request2.GetRequestStream();
+            dataStream2.Write(rawData2, 0, rawData2.Length);
+            dataStream2.Close();
+
+            // reading data
+            var stream = new StreamReader(response.GetResponseStream());
+            stream.Close();
+            
+
+        }
+    }
+}
