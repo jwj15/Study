1eb8712d1bf1661490c0630b64a93e901c1b9d88
jjang <jangwookjin100@gmail.com>
2022-07-04 22:25:13 +0900

    php ch01

---
+++ PHP/ch01.php
@@ -0,0 +1,19 @@
+<?php // 시작Tag
+// echo 출력 명령어
+echo "이곳에 코드 작성";
+echo "나눠서 적어도 바로 글자는 이어진다";
+echo "<br>";
+echo 1234;
+echo "<br>";
+echo true; // true는 1로 표시되네?
+echo "<br>";
+// 주석 스타일1
+/* 주석 스타일2*/
+# 주석 스타일3
+echo "<!-- html주석 표시-->";
+echo phpinfo(); // php 정보를 표시
+
+
+
+// 종료 Tag 생략가능 
+?> 
